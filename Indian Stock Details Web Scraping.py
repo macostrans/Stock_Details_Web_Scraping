0b9a9037f306237bdeacb822ec529aab23e22e53
Kannan Thangaraju <kannanoferode@gmail.com>
2019-01-26 15:40:28 +0530

    Screener.in Web Scraping
    Complete Python code for scraping Indian Stock Market Cap Data from Screenr.in

---
+++ Indian Stock Details Web Scraping.py
@@ -0,0 +1,13 @@
+import bs4
+import requests
+"""Either use Stock code or ticker symbol which every stock has to create a list. I will use Tata Consultancy Service, 
+Ashok Leyland and ITC"""
+stock_list = ['ASHOKLEY','TCS','ITC'] #you can use a excel file or csv instead and loop. I am keeping it simple
+
+for stock in stock_list:
+    #sample URL https://www.screener.in/company/<COMPANY-CODE>
+    res = requests.get(r"https://www.screener.in/company/" + stock)#Downloading the HTML
+    res.raise_for_status()#Nothing happens if the code is exceuted succefully else throws a exception
+    soup = bs4.BeautifulSoup(res.text,"html.parser")#Save the HTML File as Soup Object
+    elems = soup.select('section:nth-of-type(1) > ul > li:nth-of-type(1) > b') #Scraping the Market Cap From the HTML
+    print("The Market Cap of " + stock + " is " + elems[0].text.strip() + " Crores") #Instead of Printing I can save it as a csv file
